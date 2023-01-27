834cb0fb1ea3b9aab6128cc07a56e3492bc61bc3
wjy0904 <wjy0904@hanmail.net>
2023-01-27 22:01:21 +0900

    level1 - 1,2,3

---
+++ src/level1/aPlusBN1000.java
@@ -0,0 +1,23 @@
+package level1;
+
+import java.util.Scanner;
+
+public class aPlusBN1000 {
+
+	public static void main(String[] args) {
+		
+		// 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+		Scanner sc = new java.util.Scanner(System.in);
+		int a = 0;
+		int b = 0;
+		
+		a = sc.nextInt();
+		b = sc.nextInt();
+		
+		System.out.println(a + b);
+		
+		sc.close();
+
+	}
+
+}
---
+++ src/level1/helloWorldN2557.java
@@ -0,0 +1,12 @@
+package level1;
+
+public class helloWorldN2557 {
+
+	public static void main(String[] args) {
+ 		
+		// Hello World!를 출력하시오.
+		System.out.print("Hello World!");
+
+	}
+
+}
